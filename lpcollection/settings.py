6a2cdc63540929ddbb76d7a1902ff45c6db2e6b4
Ville.Heiskanen <ville.heiskanen@edu.savonia.fi>
2020-03-02 20:09:11 +0200

    Toimiva tietokantayhteys

--- lpcollection/settings.py
+++ lpcollection/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'lpcollection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'Vinyyli',
+        'CLIENT': {
+            'host': 'mongodb+srv://Vinyyli:Sovellus@vinyylitietokanta-ovdst.azure.mongodb.net/test?retryWrites=true&w=majority',
+            'username': 'Vinyyli',
+            'password': 'Sovellus',
+            'authMechanism': 'SCRAM-SHA-1'
+        }
     }
 }
 
