10456cce4478b588c7bcb13c38541a979cf5589f
Heini.Saastamoinen <heini.saastamoinen@edu.savonia.fi>
2020-03-12 10:32:55 +0200

    SFC is now working

--- lpcollection/frontend/src/components/Add.vue
+++ lpcollection/frontend/src/components/Add.vue
@@ -1,57 +1,16 @@
 <template>
   <div>
-<!-- <basicform></basicform> -->
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
+    <basicform/> 
   </div>
 </template>
 <script>
-
-const axios = require('axios'); //required for ajax calls
-// import basicform from './basicform';
+import basicform from './basicform.vue';
 
 export default {
   name: "Add",
-  data() {
-    return {
-        show: true,
-        form: {
-          name: '',
-          year: '',
-          artist: '',
-        },
-        message: "",
-    };
-  },
   components: {
-    // 'basicform': basicform
+     basicform
   },
-  methods: {
-    submitForm(evt){
-         evt.preventDefault()
-        alert(JSON.stringify(this.form))
-      axios
-        .post('http://127.0.0.1:8000/catalog/addNew') //sends a message to server
-        .then(data => (this.message = data.data.data)) //this is stupid but works :P
-        .catch(error => (this.error = error))
-        .then(this.loading = false)
-    },
-    resetForm(evt){
-         evt.preventDefault()
-        // Reset our form values
-        this.form.name = ''
-        this.form.year = ''
-        this.form.artist = ''
-         // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-    }
-  }
+
 };
 </script>
-
-<!-- CSS that is limited to this element only -->
-<style scoped></style>
--- lpcollection/frontend/src/components/basicform.vue
+++ lpcollection/frontend/src/components/basicform.vue
@@ -1,14 +1,57 @@
-export default
-Vue.component('basicform', {
-  data: function () {
+<template>
+  <b-form @submit="submitForm" @reset="resetForm">
+    <b-form-group id="input-group-1" label="Record name:" label-for="input-1">
+      <b-form-input id="input-1" v-model="form.name" required placeholder="Enter record name"></b-form-input>
+    </b-form-group>
+    <b-form-group id="input-group-2" label="Year:" label-for="input-2">
+      <b-form-input id="input-2" v-model="form.year" required placeholder="Enter publishing year"></b-form-input>
+    </b-form-group>
+    <b-form-group id="input-group-3" label="Artist:" label-for="input-3">
+      <b-form-input id="input-3" v-model="form.artist" required placeholder="Enter artist name"></b-form-input>
+    </b-form-group>
+    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
+   
+  </b-form>
+</template>
+
+<script>
+const axios = require('axios'); //required for ajax calls
+const basicform = {
+  name: 'basicform',
+  data () {
     return {
-      form: {
-          name: "",
-          year: "",
-          artist: ""
-      }
-    }
+    form: {
+      name: "",
+      year: "",
+      artist: ""
+    }}
   },
-  template: '<b>Hei</b>'
-//   template: '<b-form @submit="submitForm" @reset="resetForm" v-if="show"><b-form-group id="input-group-1" label="Record name:" label-for="input-1" >        <b-form-input          id="input-1"          v-model="form.name"          required          placeholder="Enter record name"        ></b-form-input>      </b-form-group>      <b-form-group id="input-group-2" label="Year:" label-for="input-2">        <b-form-input          id="input-2"          v-model="form.year"          required          placeholder="Enter publishing year"        ></b-form-input>      </b-form-group>      <b-form-group id="input-group-3" label="Artist:" label-for="input-3">        <b-form-input          id="input-3"          v-model="form.artist"          required          placeholder="Enter artist name"        ></b-form-input>      </b-form-group>      <b-button type="submit" variant="primary">Submit</b-button>      <b-button type="reset" variant="danger">Reset</b-button>    </b-form>'
-  })
+  methods: {
+    submitForm(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+      axios
+        .post("http://127.0.0.1:8000/catalog/addNew") //sends a message to server
+        .then(data => (this.message = data.data.data)) //this is stupid but works :P
+        .catch(error => (this.error = error))
+        .then((this.loading = false));
+    },
+    resetForm(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.name = "";
+      this.form.year = "";
+      this.form.artist = "";
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
+  }
+};
+export default basicform;
+</script>
+
+
